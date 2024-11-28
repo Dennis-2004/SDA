6c4c42ef72a1d81307313325c7dcefce4a654f51
Dennis-2004 <117187297+Dennis-2004@users.noreply.github.com>
2024-11-28 12:11:55 +0100

    parcer

--- test.py
+++ test.py
@@ -3,13 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-df = pd.read_csv("data/cryptonews.csv")
+cn = pd.read_csv("data/cryptonews.csv")
+bp = pd.read_csv("data/btc.csv")
+# print(bp["date"][])
 data = []
 
-for x in df["date"]:
+for x in [cn["date"][0], cn["date"][1]]:
     data.append(int(datetime.strptime(x.split()[0], "%Y-%m-%d").timestamp()))
 
-figure=plt.figure(figsize=(8,8))
-height, _, _ = plt.hist(data, 25, density=True)
-plt.show()
+print(data)
 
+# figure=plt.figure(figsize=(8,8))
+# height, _, _ = plt.hist(data, 25, density=True)
+# plt.show()
